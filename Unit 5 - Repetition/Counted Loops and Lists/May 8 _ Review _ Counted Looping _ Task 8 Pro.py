4a39a10769fc35b7ddcf21d00e41ea19346927da
evilswirles <185973850+evilswirles@users.noreply.github.com>
2025-05-08 12:34:10 -0400

    Updated

--- Unit 5 - Repetition/Counted Loops and Lists/May 8 _ Review _ Counted Looping _ Task 8 Pro.py
+++ Unit 5 - Repetition/Counted Loops and Lists/May 8 _ Review _ Counted Looping _ Task 8 Pro.py
@@ -1,6 +1,10 @@
 ###TASK 8 - PRO###
+###TASK 8 - PRO###
+#input
 number = int(input("How many numbers do you want to add?: "))
 numbers = []
-
 for i in range(number):
-    numbers.append(input("Enter a score: "))
+    numbers.append(float(input("Enter a score: ")))
+
+#prints the numbers that you wanted to add
+print(numbers)
